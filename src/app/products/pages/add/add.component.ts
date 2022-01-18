8adfb4cdd4eb99f5263b8eb5be7a87c7acbe958c
EmanuelTejada032 <emanueltejada31295@gmail.com>
2022-01-18 10:59:51 -0400

    Add custom properties , Input setters, fieldValidation

--- src/app/products/pages/add/add.component.ts
+++ src/app/products/pages/add/add.component.ts
@@ -9,6 +9,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddComponent implements OnInit {
 
+  message: string = "This is not a required field"
+  messageColor: string = 'green';
+
+
   productsForm: FormGroup = this.formBuilder.group({
     name: ['',Validators.required]
   })
@@ -21,10 +25,20 @@ export class AddComponent implements OnInit {
 
   sendFormData(){
     console.log(this.productsForm.value);
-    
+    this.changeMessage();
   }
 
   isInvalidField(field: string): boolean{
     return this.productsForm.get(field)?.invalid || false;
+  }
+
+  changeMessageColor(){
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    this.messageColor = color;
+
+  }
+
+  changeMessage(){
+    this.message = `this is a required field ${Math.floor(Math.random() * 10)}`;
   }
 }
--- src/app/shared/directives/error-message.directive.ts
+++ src/app/shared/directives/error-message.directive.ts
@@ -1,22 +1,67 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appErrorMessage]'
 })
-export class ErrorMessageDirective implements OnInit{
+export class ErrorMessageDirective implements OnInit, OnChanges{
 
   htmlElement!: ElementRef<HTMLElement>;
-  @Input() color: string = 'red';
+  private _color: string = 'red'; 
+  private _message: string = "Not req field";
 
+  
+  @Input() set color(value : string){
+    this.setColor();
+    this._color = value;
+  }
+  
+  @Input() set message(value : string){
+    this.setMessage();
+    this._message = value;
+  }
+  
+  @Input() set isValid(isValid : boolean){
+    if(!isValid){
+      this.htmlElement.nativeElement.classList.add('hidden');
+    }else{
+      this.htmlElement.nativeElement.classList.remove('hidden');
+    }
+
+  };
   constructor(private element: ElementRef<HTMLElement>) {
     this.htmlElement =  element;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //Not a great way since a directive can have a lot of inputs
+    // const {message, color} = changes;
+    // if(message){
+    //   this.htmlElement.nativeElement.innerText = message.currentValue;
+    // }
+
+    // if(color){
+    //   this.htmlElement.nativeElement.style.color = color.currentValue;
+    // }
+
   }
   
   ngOnInit(): void {
     this.setColor();
+    this.setMessage();
+    this.setCssClass();
+  }
+
+  setCssClass() {
+   this.htmlElement.nativeElement.className = "form-text";
   }
 
   setColor(): void {
-    this.htmlElement.nativeElement.style.color = this.color;
+    this.htmlElement.nativeElement.style.color = this._color;
   }
+
+  setMessage(): void {
+    this.htmlElement.nativeElement.innerText = this._message;
+  }
+
+
 }
